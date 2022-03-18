a2973acb6b749e6f79afd8af31fa3de7980a351f
Aleksey Koleznev <fc.fenomen@mail.ru>
2022-03-18 16:30:57 +0700

    upload url

--- const.js
+++ const.js
@@ -1,8 +1,8 @@
-// herocku
-export const httpServer = "https://admin.musictherapy.by";
-
 // prodaction
-// export const httpServer = "https://musictherapy96.herokuapp.com";
+// export const httpServer = "https://admin.musictherapy.by";
+
+// herocku
+export const httpServer = "https://musictherapy96.herokuapp.com";
 
 // localhost
 // export const httpServer = "http://192.168.0.140:5000";
