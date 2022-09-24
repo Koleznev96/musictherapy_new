ca226f7b95aaa5d835157e12a64f59245f027327
Aleksey Koleznev <fc.fenomen@mail.ru>
2022-09-25 00:27:49 +0700

    dubug version 5.0

--- src/screens/profile/components/ProfileData.js
+++ src/screens/profile/components/ProfileData.js
@@ -20,72 +20,75 @@ export const ProfileData = ({
 }) => {
   const auth = useContext(AuthContext);
   return (
-    <View style={styles.block_l}>
-      <InputFull
-        translations={auth.translations}
-        data={{
-          value: form?.name,
-          name: 'name',
-          change: setRoot,
-          placeholder: checkLanguageConst('Имя', auth.translations),
-          error: errorField?.name,
-        }}
-      />
-      <InputFull
-        translations={auth.translations}
-        data={{
-          value: form?.fullName,
-          name: 'fullName',
-          change: setRoot,
-          placeholder: checkLanguageConst('Фамилия', auth.translations),
-          error: errorField?.fullName,
-        }}
-      />
-      <InputFull
-        translations={auth.translations}
-        data={{
-          value: form?.telephone,
-          name: 'telephone',
-          change: setRoot,
-          placeholder: checkLanguageConst('Телефон', auth.translations),
-          error: errorField?.telephone,
-        }}
-      />
-      <InputFull
-        translations={auth.translations}
-        data={{
-          value: form?.email,
-          name: 'email',
-          change: setRoot,
-          placeholder: checkLanguageConst('E-mail', auth.translations),
-          error: errorField?.email,
-        }}
-      />
+    <>
+      <View style={styles.block_l}>
+        <InputFull
+          translations={auth.translations}
+          data={{
+            value: form?.name,
+            name: 'name',
+            change: setRoot,
+            placeholder: checkLanguageConst('Имя', auth.translations),
+            error: errorField?.name,
+          }}
+        />
+        <InputFull
+          translations={auth.translations}
+          data={{
+            value: form?.fullName,
+            name: 'fullName',
+            change: setRoot,
+            placeholder: checkLanguageConst('Фамилия', auth.translations),
+            error: errorField?.fullName,
+          }}
+        />
+        <InputFull
+          translations={auth.translations}
+          data={{
+            value: form?.telephone,
+            name: 'telephone',
+            change: setRoot,
+            placeholder: checkLanguageConst('Телефон', auth.translations),
+            error: errorField?.telephone,
+          }}
+        />
+        <InputFull
+          translations={auth.translations}
+          data={{
+            value: form?.email,
+            name: 'email',
+            change: setRoot,
+            placeholder: checkLanguageConst('E-mail', auth.translations),
+            error: errorField?.email,
+          }}
+        />
 
-      <ButtonFull
-        data={{
-          value: checkLanguageConst('Сохранить изменения', auth.translations),
-          change: saveHandler,
-          styles_text: {color: !statusNewData ? ColorsStyles.colorHr : '#FFF'},
-          styles: !statusNewData
-            ? {
-                marginTop: 30,
-                backgroundColor: 'rgba(0, 0, 0, 0)',
-                borderWidth: 1,
-                borderColor: ColorsStyles.colorHr,
-              }
-            : {marginTop: 40},
-        }}
-      />
-      <ButtonFull
-        data={{
-          value: checkLanguageConst('Выйти из аккаунта', auth.translations),
-          change: logoutHandler,
-          styles: {marginTop: 10},
-        }}
-      />
+        <ButtonFull
+          data={{
+            value: checkLanguageConst('Сохранить изменения', auth.translations),
+            change: saveHandler,
+            styles_text: {
+              color: !statusNewData ? ColorsStyles.colorHr : '#FFF',
+            },
+            styles: !statusNewData
+              ? {
+                  marginTop: 30,
+                  backgroundColor: 'rgba(0, 0, 0, 0)',
+                  borderWidth: 1,
+                  borderColor: ColorsStyles.colorHr,
+                }
+              : {marginTop: 40},
+          }}
+        />
+        <ButtonFull
+          data={{
+            value: checkLanguageConst('Выйти из аккаунта', auth.translations),
+            change: logoutHandler,
+            styles: {marginTop: 10, marginBottom: 60},
+          }}
+        />
 
-      <ButtonFull
+        {/* <ButtonFull
         data={{
           value: checkLanguageConst('Удалить аккаунт', auth.translations),
           change: deleteHandler,
@@ -97,7 +100,42 @@ export const ProfileData = ({
             borderColor: ColorsStyles.colorHr,
           },
         }}
-      />
-    </View>
+      /> */}
+      </View>
+      <View
+        style={{
+          // width: '100%',
+          position: 'absolute',
+          bottom: -10,
+          left: 0,
+          right: 0,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <TouchableOpacity
+          onPress={() => deleteHandler()}
+          style={{
+            paddingBottom: 22,
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingTop: 12,
+            backgroundColor: '#1D293C',
+          }}>
+          <Text
+            style={[
+              GlobalStyle.CustomFontRegular,
+              styles.button_text_delete_ac,
+            ]}>
+            {checkLanguageConst(
+              'Удалить аккаунт',
+              auth.translations,
+            ).toLowerCase()}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
