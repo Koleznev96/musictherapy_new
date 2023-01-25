9044aa6740380e62e52b1a21a305fb3a8a06f405
Aleksey Koleznev <fc.fenomen@mail.ru>
2023-01-26 04:32:17 +0700

    Add file-conguration Settings

--- Settings/const.js
+++ Settings/const.js
@@ -1,10 +1,10 @@
 // prodaction
-// export const httpServer = 'https://admin.musictherapy.by';
+export const httpServer = 'https://admin.musictherapy.by';
 
 // herocku
 // export const httpServer = "https://musictherapy96.herokuapp.com";
 
 // localhost
-export const httpServer = 'http://192.168.0.167:5000';
+// export const httpServer = 'http://192.168.0.167:5000';
 
 export const appVersion = '1'; // 18.01.2023
